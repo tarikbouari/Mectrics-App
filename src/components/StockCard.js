a79222c65dd2e7a45f535fbc0daac6db22e0ec6e
Rikstone <tarikbouari@gmail.com>
2022-10-12 19:47:33 +0000

    Create coin props

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FaArrowCircleRight } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-const Card = () => (
-  <div>
-    <h1> Europe</h1>
-    <spann> 6600</spann>
-  </div>
-);
+const Card = (prop) => {
+  const { stockName, image } = prop;
+
+  return (
+    <div className="cardCoin">
+      <div>
+        <img src={image} alt="coin-logo" className="coinlogo" />
+      </div>
+      <div className="symbol">
+        <FaArrowCircleRight className="arrow-right" />
+        <span>{stockName}</span>
+      </div>
+
+    </div>
+  );
+};
+
+Card.propTypes = {
+  // eslint-disable-next-line
+  stockName: PropTypes.string.isRequired,
+  // eslint-disable-next-line
+  image: PropTypes.string.isRequired,
+};
 export default Card;
