457bba922d991afe1725f1d594f71a7164ab339b
Rikstone <tarikbouari@gmail.com>
2022-10-12 19:44:58 +0000

    Create route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router';
 import Nav from './components/Nav';
-import Home from './components/Home';
+import Home from './components/StockList';
+import Details from './components/Details';
 
 function App() {
   return (
     <div>
       <Nav />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details" element={<Details />} />
+      </Routes>
     </div>
   );
 }
