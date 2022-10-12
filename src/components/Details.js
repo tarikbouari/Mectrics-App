bebf88d6972e6bd020958e9efca307820d7f156c
Rikstone <tarikbouari@gmail.com>
2022-10-12 19:46:43 +0000

    Create link to home page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Details = () => (
-  <div />
+  <div>
+    <h1>Hello</h1>
+    <NavLink to="/">Back</NavLink>
+  </div>
 );
 
 export default Details;
