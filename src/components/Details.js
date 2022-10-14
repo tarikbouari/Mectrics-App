68291ab9136cb22664788f15a2cfa0e822743b1f
Rikstone <tarikbouari@gmail.com>
2022-10-14 23:40:29 +0000

    Update component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { FaArrowCircleLeft } from 'react-icons/fa';
 
-const Details = () => (
-  <div>
-    <h1>Hello</h1>
-    <NavLink to="/">Back</NavLink>
-  </div>
-);
+const Details = () => {
+  const detailsCoins = useSelector((state) => state.detail.details.coin);
+  const price = parseInt(detailsCoins.price, 10).toFixed(4).toString();
+
+  const {
+    stockName: __,
+    sparkline: ___,
+    image: ____,
+    color: _____,
+    uuid: ______,
+    ...rest
+  } = detailsCoins;
+
+  return (
+    <>
+      <section className="container my-3">
+        <NavLink to="/">
+          <FaArrowCircleLeft className="arrow-left" />
+        </NavLink>
+        <div className="head-item">
+          <img src={detailsCoins.iconUrl} alt={detailsCoins.name} />
+          <div>
+            <h2 className="my-0">
+              {detailsCoins.name}
+            </h2>
+            <span className="text-center">{price}</span>
+          </div>
+        </div>
+      </section>
+
+      <ul className="detail-List ">
+        {Object.entries(rest).map(([key, value]) => (
+          <li className="list-item" key={key}>
+            {key}
+            {' '}
+            <span className="SpanValue">{value}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Details;
