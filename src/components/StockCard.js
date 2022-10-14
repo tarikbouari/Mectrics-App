6e54556fa07b4ecdcc5b7e9065c261e3bbafbe3e
Rikstone <tarikbouari@gmail.com>
2022-10-14 23:41:34 +0000

    Update stockCard  component

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,28 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaArrowCircleRight } from 'react-icons/fa';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { getDetails } from '../redux/Details/coinsDetails';
 
-const Card = (prop) => {
-  const { stockName, image } = prop;
+const Card = (props) => {
+  // const { stockName, image, price, symbol } = prop;
+
+  Card.propTypes = {
+    coin: propTypes.shape({
+      name: propTypes.string.isRequired,
+      symbol: propTypes.string,
+      price: propTypes.string,
+      iconUrl: propTypes.string.isRequired,
+    }).isRequired,
+  };
+
+  const dispatch = useDispatch();
+  const handleDetails = () => {
+    dispatch(getDetails(props));
+    console.log(props);
+  };
 
   return (
     <div className="cardCoin">
       <div>
-        <img src={image} alt="coin-logo" className="coinlogo" />
+        <img src={props.coin.iconUrl} alt="coin-logo" className="coinlogo" />
       </div>
       <div className="symbol">
-        <FaArrowCircleRight className="arrow-right" />
-        <span>{stockName}</span>
+        <Link to="details" onClick={handleDetails}>
+          {' '}
+          <FaArrowCircleRight className="arrow-right" />
+          {' '}
+        </Link>
+        <span>{props.coin.name}</span>
       </div>
-
     </div>
   );
 };
 
-Card.propTypes = {
-  // eslint-disable-next-line
-  stockName: PropTypes.string.isRequired,
-  // eslint-disable-next-line
-  image: PropTypes.string.isRequired,
-};
+// Card.propTypes = {
+//   // eslint-disable-next-line
+//   stockName: propTypes.string.isRequired,
+//   // eslint-disable-next-line
+//   image: propTypes.string.isRequired,
+//   // eslint-disable-next-line
+//   price: propTypes.string,
+//   // eslint-disable-next-line
+//   symbol: propTypes.string,
+// };
 export default Card;
