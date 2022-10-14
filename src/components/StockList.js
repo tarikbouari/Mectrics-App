ec0bcc5b78f4ab30a16482658407ab77072840bb
Rikstone <tarikbouari@gmail.com>
2022-10-14 10:43:49 +0000

    src/redux/SotckActions.js/stocks.js

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -2,9 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getStock } from '../redux/SotckActions.js/stocks';
 import Card from './StockCard';
+import { getCategory } from '../redux/Category/category';
 
 const Home = () => {
   const stocks = useSelector((state) => state.stock);
+  const categoriesState = useSelector((state) => state.category);
+  console.log(categoriesState);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,26 +16,114 @@ const Home = () => {
     }
   }, []);
 
+  dispatch(getCategory());
+ 
+
+  const getTopTen = stocks.filter((item) => item.rank < 10);
+  const getTopGainers = stocks.filter(
+    (item) => item.rank > 10 && item.rank < 21,
+  );
+
+  const getTopLosers = stocks.filter((item) => item.marketCap < 1870280014);
+
+  const handleOption = (e) => {
+    e.preventDefault();
+    const category = e.target.value;
+    dispatch(getCategory(category));
+    console.log(category);
+  };
+
   return (
-    <div className="container p-3 homeContainer">
-      <h1 className="hi"> WELCOME TO CryptoCoin</h1>
-      <div>
-        {console.log(stocks)}
-        <h3 className="py-3">Whatchlist</h3>
-        <div className="container containerCard">
-          {stocks.filter((item) => item.rank < 10)
-            .map((item) => (
-              <Card
-                key={item.uuid}
-                stockName={item.symbol}
-                image={item.iconUrl}
-              />
-            ))}
+    <>
+      <div className="container p-3 my-3 homeContainer">
+        <h1 className="hi"> WELCOME TO CryptoCoin</h1>
+        <div>
+          {console.log(stocks)}
+          <h3 className="py-3">Whatchlist</h3>
+          <div className="category">
+            <form
+              className="form"
+              name="category"
+              id="category"
+              onChange={handleOption}
+            >
+              <select>
+                <option value="All"> All crypto</option>
+                <option value="Top10"> Top 10</option>
+                <option value="Gainers"> Top Gainers</option>
+                <option value="Losers">Top Losers</option>
+              </select>
+            </form>
+          </div>
+          {categoriesState.category === 'Top10' && (
+            <section className="container containerCard">
+              <h2 className="title">Most Popular Coins</h2>
+              {getTopTen.map((item) => (
+                <Card
+                  key={item.uuid}
+                  stockName={item.symbol}
+                  image={item.iconUrl}
+                />
+              ))}
+            </section>
+          )}
+
+          {categoriesState.category === 'Gainers' && (
+            <section className="container containerCard">
+              <h2 className="title">Top Gainers</h2>
+              {getTopGainers.map((item) => (
+                <Card
+                  key={item.uuid}
+                  stockName={item.symbol}
+                  image={item.iconUrl}
+                />
+              ))}
+            </section>
+          )}
+
+          {categoriesState.category === 'Losers' && (
+            <section className="container containerCard">
+              <h2 className="title">Top Losers</h2>
+              {getTopLosers.map((item) => (
+                <Card
+                  key={item.uuid}
+                  stockName={item.symbol}
+                  image={item.iconUrl}
+                />
+              ))}
+            </section>
+          )}
+          {categoriesState.category === 'All' && (
+            <section className="container containerCard">
+              <h2 className="title">All coins</h2>
+              {stocks.map((item) => (
+                <Card
+                  key={item.uuid}
+                  stockName={item.symbol}
+                  image={item.iconUrl}
+                />
+              ))}
+            </section>
+          )}
+
+          {/* {stocks && (
+            <section className="container containerCard">
+              <h2 className="title">All coins</h2>
+              {stocks.map((item) => (
+                <Card
+                  key={item.uuid}
+                  stockName={item.symbol}
+                  image={item.iconUrl}
+                />
+              ))}
+            </section>
+          )} */}
         </div>
       </div>
-    </div>
+    </>
   );
 };
+
 export default Home;
 
-/* item.stock_exchange.acronym === 'NYSE') */
+// filter((item) => item.rank < 10)
