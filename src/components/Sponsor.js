53aeff0bd6943b33f56344fd828edb36523806ee
tarikbouari <tarikbouari@gmail.com>
2023-12-04 22:18:14 +0000

    Add sponsor component

---
+++ src/components/Sponsor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/ic.png';
+
+const Sponsor = () => (
+  <div className="">
+    <div className=" p-4 container">
+      <img src={logo} alt="logo" className="w-[70%] mx-auto" />
+    </div>
+  </div>
+);
+
+export default Sponsor;
