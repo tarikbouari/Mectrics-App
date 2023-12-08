1ff8b24c19c9755c9104003297cb3095dc0d6565
tarikbouari <tarikbouari@gmail.com>
2023-12-08 21:38:49 +0000

    Link to navigation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Service from '../components/Services';
+import Mobile from '../components/Mobile';
+import Review from '../components/Reviews';
 
 const HomePage = () => (
   <div className="">
     <Banner />
     <Service />
+    <Mobile />
+    <Review />
   </div>
 );
 
