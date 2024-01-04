0bb7437e66246f999525fd0c65363610324dd3ff
tarikbouari <tarikbouari@gmail.com>
2024-01-04 23:47:57 +0000

    Add hitorical price reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk';
 import { stockReducer } from './SotckActions/stocks';
 import CategoryReducer from './Category/category';
 import detailsReducer from './Details/coinsDetails';
+import historyReducer from './Details/coinHistory';
 
 /*  The Redux store is a single source of truth for the state of your entire application.
  It holds the application state and allows you to dispatch actions to update that state */
@@ -15,6 +16,7 @@ const reducer = combineReducers({
   stock: stockReducer,
   category: CategoryReducer,
   detail: detailsReducer,
+  history: historyReducer,
 });
 
 const store = configureStore({ reducer }, applyMiddleware(thunk));
