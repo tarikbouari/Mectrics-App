288532c48f3d582411543746a939cc9cf211c815
tarikbouari <tarikbouari@gmail.com>
2024-01-16 17:40:11 +0000

    Fix banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import crypto from '../images/dé.png';
 // import Sponsor from './Sponsor';
 
 const Banner = () => (
-  <div className=" banner md:h-[50vh] w-full">
-    <div className="container flex items-center justify-center gap-2  p-4 text-white">
-      <div className="flex flex-col gap-4 md:w-[60%] py-8">
-        <h1 className="max-md:text-4xl md:text-[34px] max-w-[400px] text-center text-[#139AFA]">Stay Ahead of the Crypto Curve </h1>
-        <p className="max-md:text-center">
+  <div className=" banner md:h-[80vh] flex w-full">
+    <div className="container flex items-center justify-center gap-2 md:pt-[5rem] px-4 text-white">
+      <div className="flex flex-col gap-4  md:w-[50%] py-8">
+        <h1 className="max-md:text-4xl md:text-6xl max-w-[536px]  md:text-left max-md:text-center text-[#139AFA] ">Stay Ahead of the Crypto Curve </h1>
+        <p className="max-md:text-center md:text-left max-w-[536px]">
           Dive into the heart of the digital
           currency revolution with our cutting-edge app that brings you real-time and historical
           crypto market data in one seamless experience.
@@ -24,8 +25,9 @@ const Banner = () => (
           Explore
         </Link>
       </div>
-      <div className="coin  md:p-[10rem] md:w-[60%] max-md:hidden" />
-
+      <div className="w-[50%]">
+        <img src={crypto} alt="cryto" />
+      </div>
     </div>
   </div>
 
